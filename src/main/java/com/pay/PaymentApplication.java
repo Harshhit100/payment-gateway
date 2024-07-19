210cce839e47d1f5618cfebdafa59f5b3b70b06e
Harshhit100 <me21btech11019@iith.ac.in>
2024-07-19 15:01:29 +0530

    Adding Configuration

--- src/main/java/com/pay/PaymentApplication.java
+++ src/main/java/com/pay/PaymentApplication.java
@@ -1,9 +1,11 @@
 package com.pay;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class PaymentApplication {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         System.out.print("Hello and welcome!");
     }
 }
